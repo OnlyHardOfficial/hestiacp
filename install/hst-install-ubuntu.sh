2dc03eb747b3e95a236b57d3a8deb7566883cd45
Marcelo Coutinho <marcelocoutinho_1994@hotmail.com>
2020-09-02 16:10:30 +0100

    Change repository # Validate whether installation script matches release version before continuing with install

--- install/hst-install-ubuntu.sh
+++ install/hst-install-ubuntu.sh
@@ -356,7 +356,7 @@ fi
 
 # Validate whether installation script matches release version before continuing with install
 if [ -z "$withdebs" ] || [ ! -d "$withdebs" ]; then
-    release_branch_ver=$(curl -s https://raw.githubusercontent.com/hestiacp/hestiacp/release/src/deb/hestia/control |grep "Version:" |awk '{print $2}')
+    release_branch_ver=$(curl -s https://raw.githubusercontent.com/OnlyHardOfficial/hestiacp/release/src/deb/hestia/control |grep "Version:" |awk '{print $2}')
     if [ "$HESTIA_INSTALL_VER" != "$release_branch_ver" ]; then
         echo
         echo -e "\e[91mInstallation aborted\e[0m"
