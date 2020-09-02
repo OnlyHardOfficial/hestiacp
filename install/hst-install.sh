4ea6c071ac1fcb13d6fe7ce6195ba806640b2308
Marcelo Coutinho <marcelocoutinho_1994@hotmail.com>
2020-09-02 15:37:22 +0100

    comment #echo "lsb_release is currently not installed, please install it:"             #echo "apt-get update && apt-get install -y lsb-release && apt-get clean all"             #exit 1

--- install/hst-install.sh
+++ install/hst-install.sh
@@ -45,9 +45,9 @@ if [ -e "/etc/os-release" ]; then
             VERSION='ubuntu'            
         else
             runlsb_release
-            echo "lsb_release is currently not installed, please install it:"
-            echo "apt-get update && apt-get install -y lsb-release && apt-get clean all"
-            exit 1
+            #echo "lsb_release is currently not installed, please install it:"
+            #echo "apt-get update && apt-get install -y lsb-release && apt-get clean all"
+            #exit 1
         fi
     elif [ "$type" = "debian" ]; then
         release=$(cat /etc/debian_version|grep -o "[0-9]\{1,2\}"|head -n1)
