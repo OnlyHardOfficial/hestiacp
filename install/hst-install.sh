5b02c00ae252b7b9f7fac393c5fb911d49c876ac
Marcelo Coutinho <marcelocoutinho_1994@hotmail.com>
2020-09-02 16:01:35 +0100

    minor changes

--- install/hst-install.sh
+++ install/hst-install.sh
@@ -79,7 +79,7 @@ fi
 check_wget_curl(){
     # Check wget
     if [ -e '/usr/bin/wget' ]; then
-        wget -q https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh -O hst-install-$type.sh
+        wget -q https://raw.githubusercontent.com/OnlyHardOfficial/hestiacp/main/install/hst-install-$type.sh -O hst-install-$type.sh
         if [ "$?" -eq '0' ]; then
             bash hst-install-$type.sh $*
             exit
@@ -91,7 +91,7 @@ check_wget_curl(){
 
     # Check curl
     if [ -e '/usr/bin/curl' ]; then
-        curl -s -O https://raw.githubusercontent.com/hestiacp/hestiacp/release/install/hst-install-$type.sh
+        curl -s -O https://raw.githubusercontent.com/OnlyHardOfficial/hestiacp/main/install/hst-install-$type.sh
         if [ "$?" -eq '0' ]; then
             bash hst-install-$type.sh $*
             exit
